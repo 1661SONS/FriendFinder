43125e72f72abd9dd4b8d0ca0e5cb541a67cba23
1661sons <julia.kerouac@gmail.com>
2018-08-14 02:04:11 -0400

    added friends array and data

---
+++ app/data/friends.js
@@ -0,0 +1,44 @@
+var friendsArray = [
+    {
+        name: 'Future',
+        photo: 'https://68.media.tumblr.com/354c0adefe9743c987cd7bfaeda3656d/tumblr_ooxp9zn7hA1qbvkmso1_500.gif',
+        scores: ['5','1','5','1','5','5','5','1','3','5']
+    }, {
+        name: 'Justin Bieber',
+        photo: 'http://www.vistazo.com/sites/default/files/field/image/2016/06/09/justinbieber.jpg',
+        scores: ['4','1','2','2','2','1','5','1','5','5']
+    }, {
+        name: 'Drake',
+        photo: 'https://studybreaks.com/wp-content/uploads/2018/01/Drake-1.jpg',
+        scores: ['5','1','5','1','4','1','3','3','5','3']
+    }, {
+        name: 'Lil Pump',
+        photo: 'https://pbs.twimg.com/profile_images/1027229152942780419/hxgWeljt_400x400.jpg',
+        scores: ['2','1','1','1','1','5','1','5','5','5']
+    }, {
+        name: 'Frank Ocean',
+        photo: 'https://data.whicdn.com/images/300272179/large.jpg',
+        scores: ['5','1','5','1','5','1','5','3','1','2']
+    }, {
+        name: 'SZA',
+        photo: 'http://s3.amazonaws.com/hiphopdx-production/2017/05/170525-SZA-IG-800x600.jpg',
+        scores: ['4','2','4','2','4','2','4','3','2','1']
+    }, {
+        name: 'Tekashi69',
+        photo: 'http://celebrityinsider.org/wp-content/uploads/2018/07/Tekashi-69-Was-Arrested-On-An-Outstanding-Warrant-In-Houston-For-His-Alleged-Involvement-In-a-Mall-Attack.jpg',
+        scores: ['1','1','1','1','1','5','1','5','5','5']
+    }, {
+        name: 'Kehlani',
+        photo: 'https://i1.sndcdn.com/avatars-000337949390-ckluqd-t500x500.jpg',
+        scores: ['4','2','4','2','4','2','4','3','2','5']
+    }, {
+        name: 'J. Cole',
+        photo: 'https://www.billboard.com/files/styles/article_main_image/public/media/j-cole-2016-abg-billboard-1548.jpg',
+        scores: ['5','1','5','1','5','1','5','3','1','3']
+    }, {
+        name: 'Syd',
+        photo: 'http://epmgaa.media.clients.ellingtoncms.com/img/photos/2017/01/24/Syd_t750x550.jpg',
+        scores: ['4','3','4','1','5','1','5','3','1','2']
+    }
+];
+module.exports = friendsArray;
